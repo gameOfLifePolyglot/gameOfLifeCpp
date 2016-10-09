787a1a357296a9794af5367f32b7894fce7698db
ghostbuster91 <kghost0@gmail.com>
2016-10-09 21:05:07 +0200

    [refactoring] use test fixture to avoid code duplication

--- test/game_test.cpp
+++ test/game_test.cpp
@@ -1,20 +1,30 @@
 #include "gtest/gtest.h"
 #include "game.h"
 
-TEST(game_test, should_game_not_be_empty_after_adding_life) {
-    Game game;
+struct GameTest : testing::Test {
 
-    game.addLife(Life(0, 0));
+    Game *game;
 
-    ASSERT_TRUE(game.isAlive(Life(0, 0)));
+    GameTest() {
+        game = new Game();
+    }
+
+    ~GameTest() {
+        delete game;
+    }
+};
+
+TEST_F(GameTest, should_game_not_be_empty_after_adding_life) {
+    game->addLife(Life(0, 0));
+
+    ASSERT_TRUE(game->isAlive(Life(0, 0)));
 }
 
 
-TEST(game_test, should_alone_life_die_after_tick) {
-    Game game;
-    game.addLife(Life(0, 0));
+TEST_F(GameTest, should_alone_life_die_after_tick) {
+    game->addLife(Life(0, 0));
 
-    game.tick();
+    game->tick();
 
-    ASSERT_FALSE(game.isAlive(Life(0, 0)));
+    ASSERT_FALSE(game->isAlive(Life(0, 0)));
 }
