c4fe9610e263edeb89c743ea31c1a6c32ffd1b73
ghostbuster91 <kghost0@gmail.com>
2016-10-09 23:14:04 +0200

    Save lives which has three neighbours

--- test/game_test.cpp
+++ test/game_test.cpp
@@ -38,3 +38,14 @@ TEST_F(GameTest, should_life_not_die_after_tick_when_it_has_two_neighbours) {
 
     ASSERT_TRUE(game->isAlive(Life(0, 0)));
 }
+
+TEST_F(GameTest, should_life_not_die_after_tick_when_it_has_three_neighbours) {
+    game->addLife(Life(0, 0));
+    game->addLife(Life(1, 0));
+    game->addLife(Life(-1, 0));
+    game->addLife(Life(-1, 1));
+
+    game->tick();
+
+    ASSERT_TRUE(game->isAlive(Life(0, 0)));
+}
