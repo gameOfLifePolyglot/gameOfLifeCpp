e878b1e7958a38f4f25f051a7b588b4804fdec1c
ghostbuster91 <kghost0@gmail.com>
2016-10-09 23:32:46 +0200

    [refactoring] make tests more readable

--- test/game_test.cpp
+++ test/game_test.cpp
@@ -7,21 +7,34 @@ struct GameTest : testing::Test {
 
     GameTest() {
         game = new Game();
+        createSingleLife(0, 0);
     }
 
-    void addTwoNeighbours(){
-        game->addLife(Life(1, 0));
-        game->addLife(Life(-1, 0));
+    void createSingleLife(int x, int y) {
+        game->addLife(Life(x, y));
     }
 
-    void addThreeNeighbours(){
+    void assertIsAlive() {
+        ASSERT_TRUE(game->isAlive(Life(0, 0)));
+    }
+
+    void assertIsDead() {
+        ASSERT_FALSE(game->isAlive(Life(0, 0)));
+    }
+
+    void addTwoNeighbours() {
+        createSingleLife(1, 0);
+        createSingleLife(-1, 0);
+    }
+
+    void addThreeNeighbours() {
         addTwoNeighbours();
-        game->addLife(Life(-1, 1));
+        createSingleLife(-1, 1);
     }
 
-    void addFourNeighbours(){
+    void addFourNeighbours() {
         addThreeNeighbours();
-        game->addLife(Life(1, 1));
+        createSingleLife(1, 1);
     }
 
     ~GameTest() {
@@ -30,43 +43,36 @@ struct GameTest : testing::Test {
 };
 
 TEST_F(GameTest, should_game_not_be_empty_after_adding_life) {
-    game->addLife(Life(0, 0));
-
-    ASSERT_TRUE(game->isAlive(Life(0, 0)));
+    assertIsAlive();
 }
 
 
 TEST_F(GameTest, should_alone_life_die_after_tick) {
-    game->addLife(Life(0, 0));
-
     game->tick();
 
-    ASSERT_FALSE(game->isAlive(Life(0, 0)));
+    assertIsDead();
 }
 
 TEST_F(GameTest, should_life_not_die_after_tick_when_it_has_two_neighbours) {
-    game->addLife(Life(0, 0));
     addTwoNeighbours();
 
     game->tick();
 
-    ASSERT_TRUE(game->isAlive(Life(0, 0)));
+    assertIsAlive();
 }
 
 TEST_F(GameTest, should_life_not_die_after_tick_when_it_has_three_neighbours) {
-    game->addLife(Life(0, 0));
     addThreeNeighbours();
 
     game->tick();
 
-    ASSERT_TRUE(game->isAlive(Life(0, 0)));
+    assertIsAlive();
 }
 
 TEST_F(GameTest, should_life_die_after_tick_when_it_has_more_then_three_neighbours) {
-    game->addLife(Life(0, 0));
     addFourNeighbours();
 
     game->tick();
 
-    ASSERT_FALSE(game->isAlive(Life(0, 0)));
+    assertIsDead();
 }
