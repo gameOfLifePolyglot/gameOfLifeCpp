cbfb5fe249f8393687ef56133247f0abf8f2392a
ghostbuster91 <kghost0@gmail.com>
2016-10-11 21:51:56 +0200

    [refactoring] Remove redundant functions

--- include/collection_utils.h
+++ include/collection_utils.h
@@ -2,18 +2,6 @@
 #include <algorithm>
 #include <iterator>
 
-template<class TInputIterator, class T>
-inline
-bool contains(TInputIterator first, TInputIterator last, const T &value) {
-    return std::find(first, last, value) != last;
-}
-
-template<class TContainer, class T>
-inline
-bool contains(const TContainer &container, const T &value) {
-    return contains(std::begin(container), std::end(container), value);
-}
-
 template<class T>
 inline
 bool contains(const std::set<T> &container, const T &value) {
